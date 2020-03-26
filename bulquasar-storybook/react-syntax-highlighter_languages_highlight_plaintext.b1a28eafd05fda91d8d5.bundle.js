c6dc9956ad2dd34e5076a8eac4df247eb9bf82f2
morco.mail@gmail.com <morco.mail@gmail.com>
2020-03-26 11:32:01 +0200

    Updated storybook with addons

---
+++ bulquasar-storybook/react-syntax-highlighter_languages_highlight_plaintext.b1a28eafd05fda91d8d5.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[121],{2811:function(module,exports){module.exports=function(hljs){return{disableAutodetect:!0}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_plaintext.b1a28eafd05fda91d8d5.bundle.js.map
---
+++ bulquasar-storybook/react-syntax-highlighter_languages_highlight_vbscriptHtml.b1a28eafd05fda91d8d5.bundle.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[170],{2860:function(module,exports){module.exports=function(hljs){return{subLanguage:"xml",contains:[{begin:"<%",end:"%>",subLanguage:"vbscript"}]}}}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_highlight_vbscriptHtml.b1a28eafd05fda91d8d5.bundle.js.map
